ee23394f55538a6f2fa9b86ad88a88351e2ab586
MilanOfc <antsyrev@gmail.com>
2023-11-23 02:04:54 +0100

    Функция get_weather теперь возвращает не объект, исправлено README

--- weatherapp.py
+++ weatherapp.py
@@ -4,17 +4,16 @@ import requests
 def get_weather(place, keys):
     """
     Get weather for given place, with given settings.
-    :return: requests.get() object
     """
     url_template = "https://wttr.in/{}"
     url = url_template.format(place)
     response = requests.get(url, params=keys)
     response.raise_for_status()
-    return response
+    return response.text
 
 
 if __name__ == '__main__':
     places = ("London", "SVO", "Череповец")
-    settings = {'n': '', 'T': '', 'q': '', 'M': '', 'lang': 'ru'}  # for more info look https://wttr.in/:help
+    settings = {'n': '', 'T': '', 'q': '', 'M': '', 'lang': 'ru'}
     for place in places:
-        print(get_weather(place, settings).text)
+        print(get_weather(place, settings))
